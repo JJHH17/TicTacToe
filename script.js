696ab2ec8d04b15cc927ab08fb445bd7dbb96c90
James Hatfield <james.hatfield17@icloud.com>
2025-02-16 20:52:15 +0000

    Add player icons to player obejct

--- script.js
+++ script.js
@@ -6,12 +6,16 @@ function player(player1, player2) {
 
     // Manages scores
     let player1Score = 0;
+    const player1Icon = 'x';
     const getPlayer1Score = () => player1Score;
     const player1Point = () => player1Score++;
 
     let player2Score = 0;
+    const player2Icon = 'o';
     const getPlayer2Score = () => player1Score;
     const player2Point = () => player2Score++;
 
-    return {player1, player2, player1Name, player2Name, getPlayer1Score, player1Point, getPlayer2Score, player2Point};
+    return {player1, player2, player1Name, player2Name, getPlayer1Score, player1Point, getPlayer2Score, player2Point, 
+        player1Icon, player2Icon};
 }
+
