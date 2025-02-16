163fdb9fa0811222bcd5cc3f5017bbce477ed0e4
James Hatfield <james.hatfield17@icloud.com>
2025-02-16 21:22:26 +0000

    Initialize gameboard IIFE creation

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Making a player class 
 
 function player(player1, player2) {
+    // User alerts/prompts to get this info from user
     const player1Name = player1;
     const player2Name = player2;
 
@@ -15,7 +16,29 @@ function player(player1, player2) {
     const getPlayer2Score = () => player1Score;
     const player2Point = () => player2Score++;
 
-    return {player1, player2, player1Name, player2Name, getPlayer1Score, player1Point, getPlayer2Score, player2Point, 
-        player1Icon, player2Icon};
-}
+    let drawScore = 0;
+    const getDrawScore = () => drawScore;
+    const drawScorePoint = () => drawScore++;
 
+    // Print scores to DOM 
+    // inner html to use <p> and <br> where needed here - remove this comment when done!!!
+    const scorePrint = document.querySelector("#scores");
+    scorePrint.innerHTML = `<p>Current Scores</p><p>${player1Name}: ${player1Score}</p><p>${player2Name}: 
+    ${player2Score}</p><p>Draw: ${drawScore}`;
+
+
+
+    return {player1, player2, player1Name, player2Name, getPlayer1Score, player1Point, getPlayer2Score, player2Point, 
+        player1Icon, player2Icon, drawScore, getDrawScore, drawScorePoint};
+}
+let newPlayer = player('James', 'Melissa');
+
+
+// IIFE for gameboard
+const gameboard = (function () {
+    let board = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+    ];
+})
