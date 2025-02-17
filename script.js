1a01d9ccc417ce3a20a810e30378423eeab3d81f
James Hatfield <james.hatfield17@icloud.com>
2025-02-17 08:20:51 +0000

    Add event listeners for board divs

--- script.js
+++ script.js
@@ -56,6 +56,126 @@ function gameplay() {
     } else if (player2Turn) {
         turnElement.innerHTML = `It's ${newPlayer.player2Name}'s turn!`;
     };
+
+    // add board dom elements
+    const one = document.querySelector("#one");
+    const two = document.querySelector("#two");
+    const three = document.querySelector("#three");
+    const four = document.querySelector("#four");
+    const five = document.querySelector("#five");
+    const six = document.querySelector("#six");
+    const seven = document.querySelector("#seven");
+    const eight = document.querySelector("#eight");
+    const nine = document.querySelector("#nine");
+
+    let currentPlayer = "x"; // Alternates between "X" and "O"
+    
+    // Add button click - add this next
+    one.addEventListener("click", () => {
+        if (currentPlayer == "x") {
+            one.innerHTML = "x";
+            // append to array
+            currentPlayer = 'o';
+        } else {
+            one.innerHTML = "o";
+            // append to array
+            currentPlayer = 'x';
+        }
+    });
+    two.addEventListener("click", () => {
+        if (currentPlayer == "x") {
+            two.innerHTML = "x";
+            // append to array
+            currentPlayer = 'o';
+        } else {
+            two.innerHTML = "o";
+            // append to array
+            currentPlayer = 'x';
+        }
+    });
+    three.addEventListener("click", () => {
+        if (currentPlayer == "x") {
+            three.innerHTML = "x";
+            // append to array
+            currentPlayer = 'o';
+        } else {
+            three.innerHTML = "o";
+            // append to array
+            currentPlayer = 'x';
+        }
+    });
+    four.addEventListener("click", () => {
+        if (currentPlayer == "x") {
+            four.innerHTML = "x";
+            // append to array
+            currentPlayer = 'o';
+        } else {
+            four.innerHTML = "o";
+            // append to array
+            currentPlayer = 'x';
+        }
+    });
+    five.addEventListener("click", () => {
+        if (currentPlayer == "x") {
+            five.innerHTML = "x";
+            // append to array
+            currentPlayer = 'o';
+        } else {
+            five.innerHTML = "o";
+            // append to array
+            currentPlayer = 'x';
+        }
+    });
+    six.addEventListener("click", () => {
+        if (currentPlayer == "x") {
+            six.innerHTML = "x";
+            // append to array
+            currentPlayer = 'o';
+        } else {
+            six.innerHTML = "o";
+            // append to array
+            currentPlayer = 'x';
+        }
+    });
+    seven.addEventListener("click", () => {
+        if (currentPlayer == "x") {
+            seven.innerHTML = "x";
+            // append to array
+            currentPlayer = 'o';
+        } else {
+            seven.innerHTML = "o";
+            // append to array
+            currentPlayer = 'x';
+        }
+    });
+    eight.addEventListener("click", () => {
+        if (currentPlayer == "x") {
+            eight.innerHTML = "x";
+            // append to array
+            currentPlayer = 'o';
+        } else {
+            eight.innerHTML = "o";
+            // append to array
+            currentPlayer = 'x';
+        }
+    });
+    nine.addEventListener("click", () => {
+        if (currentPlayer == "x") {
+            nine.innerHTML = "x";
+            // append to array
+            currentPlayer = 'o';
+        } else {
+            nine.innerHTML = "o";
+            // append to array
+            currentPlayer = 'x';
+        }
+    });
+
+    // add gameplay logic 
+    // If player 1 fills a row, they win
+    // else if player 2 does, they will
+    // else, draw
+
 }
 
 gameplay();
